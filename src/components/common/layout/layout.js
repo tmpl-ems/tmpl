644f7c0e4aeb64296c6844adaf3d8d98d0d1b948
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-02-14 18:48:58 +0200

    add esLint, add pageFormat State

--- src/components/common/layout/layout.js
+++ src/components/common/layout/layout.js
@@ -1,6 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const breakPoints = {
+  response: 479,
+  mobile: 480,
+  tablet: 768,
+  desktop: 1280,
+};
 
 const Layout = ({ children }) => {
+  const [pageFormat, setPageFormat] = useState('response');
+
+  useEffect(() => {
+    const onHandleResize = () => {
+      const viewport = window.innerWidth;
+      if (viewport < breakPoints.mobile && pageFormat !== 'response') {
+        setPageFormat('response');
+        return;
+      }
+      if (
+        viewport >= breakPoints.mobile &&
+        viewport < breakPoints.tablet &&
+        pageFormat !== 'mobile'
+      ) {
+        setPageFormat('mobile');
+        return;
+      }
+      if (
+        viewport >= breakPoints.tablet &&
+        viewport < breakPoints.desktop &&
+        pageFormat !== 'tablet'
+      ) {
+        setPageFormat('tablet');
+        return;
+      }
+      if (viewport >= breakPoints.desktop && pageFormat !== 'desktop') {
+        setPageFormat('desktop');
+        return;
+      }
+    };
+    window.addEventListener('resize', onHandleResize);
+    onHandleResize();
+  }, [pageFormat]);
+
   return <>{children}</>;
 };
 
