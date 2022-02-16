b3107834ba477c4d40ae49832d43e3ffa7723888
Vira Velmozhna <viravelmozhna@gmail.com>
2022-02-16 23:36:40 +0200

    test

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Example from './example/example';
+import AboutUsSection from './about/AboutUsSection';
 
 const Main = () => {
   return (
     <main>
-      <Example />
+      <AboutUsSection/>
     </main>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Layout from 'components/common/layout/layout';
-import Footer from 'components/footer/footer';
-import Header from 'components/header/header';
+// import Footer from 'components/footer/footer';
+// import Header from 'components/header/header';
 import Main from 'components/main/main';
 
 export default function indexPage() {
   return (
     <Layout>
-      <Header />
+      {/* <Header /> */}
       <Main />
-      <Footer />
+      {/* <Footer /> */}
     </Layout>
   );
 }
