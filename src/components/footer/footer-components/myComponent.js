b3cbbfb982c11f5f1693602e0bda6ec1f66c161c
Oksana Stets <oksanastets73@gmail.com>
2022-02-16 23:01:54 +0200

    add footer's markup and styles for mobile

---
+++ src/components/footer/footer-components/myComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Section from 'components/common/section/section'
+const MyComponent = () => { 
+    return (
+        <Section>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia rerum pariatur minima error ab hic exercitationem omnis saepe consectetur deleniti? Illum eos placeat optio doloribus id nisi beatae architecto accusantium quasi. Consequatur nesciunt veritatis vitae nisi beatae eius eaque deserunt pariatur maxime voluptate, possimus reprehenderit, quasi placeat blanditiis dolorum vero.</p>
+        </Section>
+    )
+}
+export default MyComponent
