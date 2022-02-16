fe40abbfffe60282b62041f5d4d1c6f648aa5e90
IhorStorozhok <storozhokigor@gmail.com>
2022-02-16 23:25:26 +0200

    second commit

--- src/components/main/how/how.js
+++ src/components/main/how/how.js
@@ -1,7 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Section from 'components/common/section/Section';
 import Video from 'components/common/video/video';
 import { PageFormatContext, format } from 'context/pageFormatContext';
+import { usePaddingsValues } from 'hooks/usePaddingsValues';
+import arrow from '../../../../src/images/how/dashed-angle-arrow.png';
+import secondaryArrow from '../../../../src/images/how/dashed-straight-arrow.png';
 
 const data = {
   ru: {
@@ -42,56 +45,104 @@ const data = {
   },
 };
 
+const styles = {
+  response: { pt: 28, pb: 40 },
+  mobile: { pt: 28, pb: 40 },
+  tablet: { pt: 60, pb: 60 },
+  desktop: { pt: 60, pb: 53 },
+};
+
+const videoStyles = {
+  common: {
+    border: 'double 2px transparent',
+    borderRadius: '16px',
+    backgroundImage: ' linear-gradient(180deg, #20c5a0 0%, #0a866a 100%)',
+    backgroundOrigin: 'border-box',
+    backgroundClip: 'content-box border-box',
+  },
+  mobile: { display: 'block', marginLeft: 'auto', marginRight: 'auto' },
+  tablet: { display: 'block' },
+};
+
 const How = () => {
   const videoUrl = 'https://www.youtube.com/embed/Ihvs5qt1Ut0';
   const videoTitle = 'YouTube video player';
+
   const pageFormat = useContext(PageFormatContext);
+  const [videoSizes, setVideoSizes] = useState({});
+  const [videoStyle, setVideoStyle] = useState({});
   const isTablet = pageFormat === format.tablet;
+  const isDesktop = pageFormat === format.desktop;
+  const isResponse = pageFormat === format.response;
+
+  useEffect(() => {
+    if (pageFormat === format.tablet) {
+      setVideoSizes({ width: 340, height: 266 });
+      setVideoStyle({ ...videoStyles.common, ...videoStyles.tablet });
+    }
+    if (pageFormat === format.desktop) {
+      setVideoSizes({ width: 608, height: 454 });
+    }
+    if (pageFormat === format.mobile) {
+      setVideoSizes({ width: 280, height: 266 });
+      setVideoStyle({ ...videoStyles.common, ...videoStyles.mobile });
+    }
+  }, [pageFormat]);
+
+  console.log(pageFormat);
+
+  const paddings = usePaddingsValues(styles, pageFormat, format);
 
   return (
     <div className="howBackgroundContainer">
       <Section
-        head={data.ru.title}
         titleHidden={true}
         titleLevel={'h3'}
         style={{ backgroundColor: '#000000' }}
+        pt={paddings.pt}
+        pb={paddings.pb}
       >
         <h3 className="howTitle">{data.ru.title}</h3>
+        <div className={isTablet || isDesktop ? 'howTabletFlexContainer' : ''}>
+          <Video
+            videoSrcURL={videoUrl}
+            videoTitle={videoTitle}
+            width={videoSizes.width}
+            height={videoSizes.height}
+            style={videoStyle}
+          />
 
-        <Video
-          videoSrcURL={videoUrl}
-          videoTitle={videoTitle}
-          width={280}
-          height={266}
-          style={{
-            border: 'double 2px transparent',
-            borderRadius: '16px',
-            backgroundImage:
-              ' linear-gradient(180deg, #20c5a0 0%, #0a866a 100%)',
-            backgroundOrigin: 'border-box',
-            backgroundClip: 'content-box border-box',
-            display: 'block',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-          }}
-        />
-
-        <div className="howTextContentContainer">
-          <ul>
-            {data.ru.content.textContent.map((el, index) => {
-              return (
-                <li key={index}>
-                  <div className={`howTextBox${index} howTextBox`}>
-                    <p className={`howNumber howNumber${index}`}>{el.number}</p>
-                    <p className={`howCommonText${index} howCommonText`}>
-                      {el.text}
-                    </p>
-                    <div className={`'howArrowBox${index}'`}></div>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
+          <div className="howTextContentContainer">
+            <ul>
+              {data.ru.content.textContent.map((el, index) => {
+                return (
+                  <li key={index}>
+                    <div className={`howTextBox${index} howTextBox`}>
+                      <p className={`howNumber howNumber${index}`}>
+                        {el.number}
+                      </p>
+                      <p className={`howCommonText${index} howCommonText`}>
+                        {el.text}
+                      </p>
+                      <div className={`howArrowBox${index} howArrowBox`}>
+                        {index < 3 && (
+                          <img
+                            src={
+                              index === 2 && pageFormat === format.tablet
+                                ? secondaryArrow
+                                : arrow
+                            }
+                            alt={'arrow'}
+                            className="arrowImage"
+                          ></img>
+                        )}
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </Section>{' '}
     </div>
