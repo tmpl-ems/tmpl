b997db8d416ef9eef7969ea7eeba94368cbcfef7
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-02-16 09:06:56 +0200

    add example

---
+++ src/components/main/myComponent/myComponent.js
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { PageFormatContext, format } from 'context/pageFormatContext';
+import { usePaddingsValues } from 'hooks/usePaddingsValues';
+
+import Section from 'components/common/section/section';
+
+const styles = {
+  response: { pt: 10, pb: 10 },
+  mobile: { pt: 20, pb: 20 },
+  tablet: { pt: 30, pb: 30 },
+  desktop: { pt: 40, pb: 40 },
+};
+
+const MyComponent = () => {
+  const pageFormat = useContext(PageFormatContext);
+
+  const pad = usePaddingsValues(styles, pageFormat, format);
+
+  return (
+    <Section
+      head="Это заголовок моей секции"
+      titleLevel="h2"
+      pt={pad.pt}
+      pb={pad.pb}
+    >
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+        consectetur tempore ut dolorum magnam qui. Voluptatem nobis a nulla
+        cupiditate! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Reprehenderit veniam distinctio, enim praesentium architecto, et, neque
+        porro itaque laudantium nemo doloremque earum soluta repellendus aliquam
+        voluptas dicta accusamus qui obcaecati.
+      </p>
+    </Section>
+  );
+};
+
+export default MyComponent;
