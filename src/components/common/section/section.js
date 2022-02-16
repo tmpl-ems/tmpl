d8786eb4efe3706178ae36a8b4efb667a05bf1a8
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-02-16 07:26:02 +0200

    change container and section components

--- src/components/common/section/section.js
+++ src/components/common/section/section.js
@@ -9,9 +9,19 @@ import Title from '../title/title';
 // titleLevel - можно задать тот уровень заголовка, который нужен для секции (h1, h2, h3 и т.д.)
 // titleHidden - оставляет заголовок, но делает его невидимым
 
-const Section = ({ head, titleLevel, titleHidden = false, children }) => {
+const Section = ({
+  head,
+  titleLevel,
+  titleHidden = false,
+  children,
+  pt,
+  pb,
+}) => {
   return (
-    <section className={section} style={{ paddingTop: '100px' }}>
+    <section
+      className={section}
+      style={{ paddingTop: `${pt}px`, paddingBottom: `${pb}px` }}
+    >
       <Container>
         {head && (
           <Title
@@ -34,4 +44,6 @@ Section.propTypes = {
   children: PropTypes.node,
   titleLevel: PropTypes.string,
   titleHidden: PropTypes.bool,
+  pt: PropTypes.number,
+  pb: PropTypes.number,
 };
--- src/components/main/example/example.js
+++ src/components/main/example/example.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 // Если в нашем компоненте нужно выполнить или нет рендеринг в зависимости от ширины экрана, берём испортируем контекст и формат. А такэе useContext
 import { PageFormatContext, format } from 'context/pageFormatContext';
@@ -24,12 +24,40 @@ const data = {
 
 const Example = () => {
   // забираем значение ширины экрана, которое высчитывается в компоненте Layout и реализуем рендеринг по условию
-
+  const [paddings, setPaddings] = useState({ pt: null, pb: null });
   const pageFormat = useContext(PageFormatContext);
   const isTablet = pageFormat === format.tablet;
 
+  useEffect(() => {
+    const getPaddings = () => {
+      switch (pageFormat) {
+        case format.response:
+          setPaddings({ pt: 20, pb: 30 });
+          break;
+        case format.mobile:
+          setPaddings({ pt: 30, pb: 0 });
+          break;
+        case format.tablet:
+          setPaddings({ pt: 40, pb: 40 });
+          break;
+        case format.desktop:
+          setPaddings({ pt: 60, pb: 80 });
+          break;
+        default:
+          return setPaddings({ pt: null, pb: null });
+      }
+    };
+    getPaddings();
+  }, [pageFormat]);
+
   return (
-    <Section head={data.ru.title} titleHidden={false} titleLevel="h4">
+    <Section
+      head={data.ru.title}
+      titleHidden={false}
+      titleLevel="h4"
+      pt={paddings.pt}
+      pb={paddings.pb}
+    >
       <p>{data.ru.content}</p>
       {isTablet && <LogoIcon width="200" height="200" />}
       <Button classType={3} type={'button'} />
