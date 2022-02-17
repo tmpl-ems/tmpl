f3baa3cf56b38d01292f825d5e2aae7941fb216c
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-02-17 13:26:44 +0200

    add markers

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,16 +5,18 @@ import { Enroll } from './EnrollSection/Enroll';
 import Advantages from './advantages/advantages';
 // import MyComponent from './myComponent/myComponent';
 import PersonalProgram from 'components/main/PersonalProgram';
+import Goals from './goals/goals';
 
 const Main = ({ onModalOpen }) => {
   return (
     <main>
       {/* <Example /> */}
-      <Advantages />
-      <Results />
+      {/* <Advantages />
+      <Results /> */}
       {/* <MyComponent /> */}
-      <PersonalProgram />
-      <Enroll onModalOpen={onModalOpen} />
+      {/* <PersonalProgram />
+      <Enroll onModalOpen={onModalOpen} /> */}
+      <Goals />
     </main>
   );
 };
