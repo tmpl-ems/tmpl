d2913757f44f7ce5f770e27ee83331c763f9568c
Gene4kaS <jannasavchenko@gmail.com>
2022-02-17 14:58:43 +0200

    advantages

--- src/components/main/example/example.js
+++ src/components/main/example/example.js
@@ -5,12 +5,11 @@ import { usePaddingsValues } from 'hooks/usePaddingsValues';
 
 // Если в нашем компоненте нужно выполнить или нет рендеринг в зависимости от ширины экрана, берём испортируем контекст и формат. А такэе useContext
 import { PageFormatContext, format } from 'context/pageFormatContext';
-import Section from 'components/common/section/Section';
+import Section from 'components/common/section/section';
 
 // Пример использования СВГ
 import LogoIcon from 'images/svg/logo.svg';
 import Button from 'components/common/button/button';
-import Advantages from '../advantages/advantages';
 
 // Пример организации данных, можете напрямую в конпонент писать, но в будущем, вероятнее всего нужно будет выносить весть текст в отдельные json файлы и струкрута будет примерно такой.
 const data = {
