2c6434268abd7fa353048d70bafd7da7df9744da
Alexey Sapr <alexeysapr@gmail.com>
2022-02-18 16:27:31 +0200

    Add LazyLoad

--- src/components/common/video/video.js
+++ src/components/common/video/video.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { createRef, useState, useEffect } from 'react';
+
 const Video = ({
   videoSrcURL,
   videoTitle,
@@ -8,9 +9,41 @@ const Video = ({
   videClassStyles,
   ...props
 }) => {
+  // const [showVideo, setShowVideo] = useState(true);
+  const [showVideo, setShowVideo] = useState(false);
+  const container = createRef();
+
+  const onVideoIntersection = entries => {
+    if (!entries || entries.length <= 0) {
+      return;
+    }
+
+    if (entries[0].isIntersecting) {
+      setShowVideo(true);
+      videoObserver.disconnect();
+    }
+  };
+
+  const videoObserver = new IntersectionObserver(onVideoIntersection, {
+    rootMargin: '100px 0px',
+    threshold: 0.25,
+  });
+
+  useEffect(() => {
+    if (window && 'IntersectionObserver' in window) {
+      if (container && container.current) {
+        videoObserver.observe(container.current);
+      }
+    } else {
+      setShowVideo(true);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [container]);
+
   return (
-    <>
-      {videoSrcURL && (
+    <div ref={container}>
+      {videoSrcURL && showVideo ? (
         <iframe
           width={width}
           height={height}
@@ -23,8 +56,8 @@ const Video = ({
           style={style}
           className={videClassStyles}
         />
-      )}
-    </>
+      ) : undefined}
+    </div>
   );
 };
 export default Video;
