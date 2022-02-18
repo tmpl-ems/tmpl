f9e9eead250766a406288f56338c844659d72e1a
LenaMazurenko <lena@rent-it.su>
2022-02-18 03:18:18 +0200

    list style

--- src/components/hero/content/content.js
+++ src/components/hero/content/content.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 // Если в нашем компоненте нужно выполнить или нет рендеринг в зависимости от ширины экрана, берём испортируем контекст и формат. А такэе useContext
-//import { PageFormatContext, format } from 'context/pageFormatContext';
+import { PageFormatContext, format } from 'context/pageFormatContext';
 import * as s from './content.module.scss';
 
 import SnakeArrowMobile from 'images/svg/header-snake-arrow-mobile.svg';
-//import SnakeArrow from 'images/svg/header-snake-arrow.svg';
+import SnakeArrow from 'images/svg/header-snake-arrow.svg';
 import DashedLine from 'images/svg/header-dashed-arc.svg';
 
 import Button from 'components/common/button/button';
 import List from './list/list';
 
 const Content = () => {
-  // забираем значение ширины экрана, которое высчитывается в компоненте Layout и реализуем рендеринг по условию
-  //const pageFormat = useContext(PageFormatContext);
-  //const isMobile = pageFormat === format.tablet;
-  //const isTablet = pageFormat === format.tablet;
-  //const isDesktop = pageFormat === format.desktop;
+  const pageFormat = useContext(PageFormatContext);
+  const isMobile = pageFormat === format.mobile;
+  const isTablet = pageFormat === format.tablet;
+  const isDesktop = pageFormat === format.desktop;
 
   return (
     <div className={s.contentBlock}>
@@ -25,8 +24,17 @@ const Content = () => {
       <p className={s.text1}>
         Создайте идеальную форму, занимаясь по 30 минут:
       </p>
-      <Button classType={3} type={'button'} />
-      <SnakeArrowMobile className={s.snakeArrowIcon} width={90} height={90} />
+      {(isDesktop || isTablet) && <List />}
+      <Button classType={1} type={'button'} />
+      {!isDesktop && !isTablet ? (
+        <SnakeArrowMobile
+          className={s.snakeArrowMobIcon}
+          width={90}
+          height={90}
+        />
+      ) : (
+        <SnakeArrow className={s.snakeArrowIcon} width={90} height={90} />
+      )}
       <p className={s.text2}>
         Записывайтесь на пробную <br />
         тренировку за
@@ -34,7 +42,7 @@ const Content = () => {
       </p>
       <p className={s.price}>750 грн.</p>
       <DashedLine className={s.dashedLine} width={181} height={22} />
-      <List />
+      {!isDesktop && !isTablet && <List />}
     </div>
   );
 };
