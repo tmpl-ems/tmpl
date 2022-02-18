02a6acf9e8584ea12e8d2c8fc48cdbe16f1c3e96
LenaMazurenko <lena@rent-it.su>
2022-02-18 17:44:00 +0200

    add img elements

--- src/components/hero/content/content.js
+++ src/components/hero/content/content.js
@@ -1,18 +1,11 @@
 import React, { useContext } from 'react';
-// Если в нашем компоненте нужно выполнить или нет рендеринг в зависимости от ширины экрана, берём испортируем контекст и формат. А такэе useContext
 import { PageFormatContext, format } from 'context/pageFormatContext';
-import * as s from './content.module.scss';
-
-import SnakeArrowMobile from 'images/svg/header-snake-arrow-mobile.svg';
-import SnakeArrow from 'images/svg/header-snake-arrow.svg';
-import DashedLine from 'images/svg/header-dashed-arc.svg';
-
 import Button from 'components/common/button/button';
 import List from './list/list';
+import * as s from './content.module.scss';
 
 const Content = () => {
   const pageFormat = useContext(PageFormatContext);
-  const isMobile = pageFormat === format.mobile;
   const isTablet = pageFormat === format.tablet;
   const isDesktop = pageFormat === format.desktop;
 
@@ -25,23 +18,21 @@ const Content = () => {
         Создайте идеальную форму, занимаясь по 30 минут:
       </p>
       {(isDesktop || isTablet) && <List />}
-      <Button classType={1} type={'button'} />
-      {!isDesktop && !isTablet ? (
-        <SnakeArrowMobile
-          className={s.snakeArrowMobIcon}
-          width={90}
-          height={90}
-        />
+      {isDesktop || isTablet ? (
+        <Button classType={1} type={'button'} />
       ) : (
-        <SnakeArrow className={s.snakeArrowIcon} width={90} height={90} />
+        <Button classType={4} type={'button'} />
       )}
-      <p className={s.text2}>
-        Записывайтесь на пробную <br />
-        тренировку за
-        <span className={s.text2_span}>750 грн.</span>
-      </p>
-      <p className={s.price}>750 грн.</p>
-      <DashedLine className={s.dashedLine} width={181} height={22} />
+
+      <div className={s.offerBlock}>
+        <p className={s.text2}>
+          Записывайтесь на пробную <br />
+          тренировку за
+          <span className={s.text2_span}>750 грн.</span>
+        </p>
+        <p className={s.price}>350 грн.</p>
+      </div>
+
       {!isDesktop && !isTablet && <List />}
     </div>
   );
