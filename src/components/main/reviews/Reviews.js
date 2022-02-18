a5d324be1f392620461cad2889a01b86de19081b
Alexey Sapr <alexeysapr@gmail.com>
2022-02-18 12:00:52 +0200

    Add Slider component, update Results, Reviews

--- src/components/main/reviews/Reviews.js
+++ src/components/main/reviews/Reviews.js
@@ -2,21 +2,13 @@ import React from 'react';
 // import * as s from './Reviews.module.scss';
 
 import Section from 'components/common/section/section';
+import Slider from 'components/common/slider/slider';
 import ReviewCard from '../reviewCard/ReviewCard';
 
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import Swiper React components
+import { SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
-import 'swiper/scss';
-import 'swiper/scss/pagination';
-
-import 'styles/vendor/_swiperStyles.scss';
-
-// import required modules
-import { Pagination, Keyboard } from 'swiper';
-
-//girls foto
+// girls foto
 import reviewsGirl_1_1x from 'images/reviews/reviews-girl1.png';
 import reviewsGirl_1_2x from 'images/reviews/reviews-girl1-x2.png';
 
@@ -26,22 +18,7 @@ import reviewsGirl_2_2x from 'images/reviews/reviews-girl2-x2.png';
 const Reviews = () => {
   return (
     <Section head={'Отзывы'} darkBackground={true} titleLevel="h4">
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={32}
-        breakpoints={{
-          768: {
-            slidesPerView: 2,
-          },
-          1280: {
-            slidesPerView: 4,
-          },
-        }}
-        pagination={true}
-        keyboard={true}
-        modules={[Pagination, Keyboard]}
-        className="reviewSwiper"
-      >
+      <Slider isPagination isReviewSlides sliderClass="reviewSwiper">
         <SwiperSlide>
           <ReviewCard
             text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mollis varius eros ac rhoncus. Phasellus volutpat sem leo, sed pulvinar lacus gravida."
@@ -86,7 +63,7 @@ const Reviews = () => {
             girlName="Анастасия"
           />
         </SwiperSlide>
-      </Swiper>
+      </Slider>
     </Section>
   );
 };
