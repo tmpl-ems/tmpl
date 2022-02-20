e36f16ad93f708c29d09a71fcf9fa036a7dec06f
LenaMazurenko <lena@rent-it.su>
2022-02-20 15:47:00 +0200

    add lang and del br

--- src/components/hero/content/list/list.js
+++ src/components/hero/content/list/list.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import * as s from './list.module.scss';
 
-const List = () => {
-  const data = {
-    ru: [
-      'Без тяжелых весов и нагрузок',
-      'Индивидуально с тренером',
-      'Прорабатывая все группы мышц',
-      'Укрепляя здоровье',
-    ],
-    ua: [],
-  };
+const List = ({ data }) => {
   return (
     <ul className={s.list}>
-      {data.ru.map((item, i) => (
+      {data.map((item, i) => (
         <li key={i} className={s.listItem}>
           {item}
         </li>
